0c935eac653d24ecb977f4a6d91126b772c57019
Yulissa Lopez <lyulissa5@gmail.com>
2024-11-30 01:48:23 -0500

    creacion carpeta utils y mover funcion gettokensas dentro de la carpeta

--- busquedas/views.py
+++ busquedas/views.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
-from .utils import get_sas_url
+from .utils.getSasUrl import get_sas_url
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+
 
 
 # Create your views here.
