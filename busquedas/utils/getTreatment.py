6106ce9b244ebd097d08bc2ad39913b331cf031f
Yulissa Lopez <lyulissa5@gmail.com>
2024-12-06 23:25:54 -0500

    Manejo error enfermedad no encontrada en BD y Retorno para planta sana

--- busquedas/utils/getTreatment.py
+++ busquedas/utils/getTreatment.py
@@ -1,18 +1,34 @@
 from enfermedades.models import Enfermedad, Tratamiento
 from enfermedades.serializers import TratamientoSerializer
 import random
+
 def get_treatment(illness):
+    print("En funcion get_treatment")
     
-    # Filtrar la enfermedad
-    illness_bd = Enfermedad.objects.get(nombre=illness)
-    print(illness_bd)
-    # Filtrar los tratamientos de esa enfermedad
-    treatments = Tratamiento.objects.filter(enfermedad_id=illness_bd.pk)
+    try:
+        # Verificar si el resultado es healthy
+        if illness == "healthy":
+             # Cuando la planta este sana no entregar tratamiento
+            return {"mensaje" : "La planta está sana y no requiere tratamiento"}
+        
+        # Filtrar la enfermedad
+        illness_bd = Enfermedad.objects.get(nombre=illness)
 
-    # convertir a un diccionario
-    serializer = TratamientoSerializer(treatments, many = True)
+        print(illness_bd)
+        # Filtrar los tratamientos de esa enfermedad
+        treatments = Tratamiento.objects.filter(enfermedad_id=illness_bd.pk)
 
-    # Asignar uno al azar
-    tratamiento = random.choice(serializer.data)
+        # convertir a un diccionario
+        serializer = TratamientoSerializer(treatments, many = True)
 
-    return tratamiento
+        # Asignar uno al azar
+        tratamiento = random.choice(serializer.data)
+
+        return tratamiento
+    except Enfermedad.DoesNotExist:
+        # Manejar error cuando la enfermedad que entregue la ia no exista
+        return {"error": f"Tratamientos para esta enfermedad: {illness} no disponibles "}
+
+
+    
+    
