b0a22f826b35e8ce87219a85db7ffe59084fc10c
Yulissa Lopez <lyulissa5@gmail.com>
2024-12-18 23:26:07 -0500

    actualizacion serializador

--- busquedas/urls.py
+++ busquedas/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
 
     # Obtiene las busquedas del usuario logeado
     path('busquedas/', BusquedaListView.as_view(), name='busqueda-list-create'),
-    
     path('busquedas/<uuid:pk>/', BusquedaDetailView.as_view(), name='busqueda-detail'),
     path('analyze-image/', AnalyzeImageView.as_view(), name='analyze_image'),
 ]
