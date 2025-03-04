d463f158ddc8b42aec52653bb7f3690fdc5d113b
Yulissa <lyulissa5@gmail.com>
2025-03-04 00:01:57 -0500

    manejo de errores en funcion geocoding.py

--- busquedas/utils/geocoding.py
+++ busquedas/utils/geocoding.py
@@ -5,25 +5,35 @@ from pprint import pprint
 key = env("OPENCAGE_KEY")
 
 def get_location_name(latitude, longitude ):
+
+    try:
+        # Importa la clase OpenCageGeocode y crea una instancia con la clave de la API
+        geocoder = OpenCageGeocode(key)
+
+        # convierte latitud y longitud en una dirección
+        results = geocoder.reverse_geocode(latitude, longitude)
+
+        # TODO si la solicitud falla o no entrega resultados, manejar la excepción
+        if not results:
+            print("Error: No se encontraron resultados.")
+            return None
+
+        # Extrae la dirección completa en formato legible
+        formatted_address = results[0].get("formatted")
+
+        if not formatted_address:
+            print("Error: Direccion vacía")
+            return None
+
+        # Divide la dirección en partes separadas por comas
+        parts = formatted_address.split(", ")
+
+        # Obtiene el segundo elemento, que generalmente corresponde al barrio
+        location_name = parts[1]
+
+        return location_name
     
-    # Importa la clase OpenCageGeocode y crea una instancia con la clave de la API
-    geocoder = OpenCageGeocode(key)
-
-    # TODO Si la api key esta vacia, devuelve None
-
-    # convierte latitud y longitud en una dirección
-    results = geocoder.reverse_geocode(latitude, longitude)
-
-    # TODO si la solicitud falla o no entrega resultados, manejar la excepción
-
-    # Extrae la dirección completa en formato legible
-    formatted_address = results[0].get("formatted")
-
-    # Divide la dirección en partes separadas por comas
-    parts = formatted_address.split(", ")
-
-    # Obtiene el segundo elemento, que generalmente corresponde al barrio
-    location_name = parts[1]
-
-    return location_name
-    
+    except Exception as e:
+        print(f"Error al obtener la ubicación: {e}")
+        return None
+        
